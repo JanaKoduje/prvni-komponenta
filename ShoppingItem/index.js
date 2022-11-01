24b3e515062a127cd1f31d7844d9c30eaec98227
JanaKoduje <jjanule@gmail.com>
2022-11-01 22:12:00 +0100

    snaha s vyberem dne

--- ShoppingItem/index.js
+++ ShoppingItem/index.js
@@ -1,6 +1,5 @@
 export const ShoppingItem = (props) => {
-  const { product, amount, done } = props;
-
+  const { id, product, amount, unit, done, day } = props;
   let checkClass = '';
   if (done) {
     checkClass = 'item__btn-done--check';
@@ -10,16 +9,19 @@ export const ShoppingItem = (props) => {
   element.classList.add('item');
   element.innerHTML = `
     <div class="item__name">${product}</div>
-    <div class="item__amount">${amount}</div>
-    <button class="item__btn-done ${checkClass}"></button>
+    <div class="item__amount">${amount} ${unit}</div>
+    <button id="checked" class="item__btn-done ${checkClass}"></button>
   `;
-  element.querySelector('button').addEventListener('click', () => {
-    element.replaceWith(ShoppingItem({ 
-      product: product,
-      amount: amount,
-      done: !done,
-    }));
-  });
+  element.querySelector('#checked').addEventListener('click', () => {
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/31/days/${day}/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ done: !done }),
+      }).then((response) => response.json())
+        .then((data) => element.replaceWith(ShoppingItem({...data.results, day})));
+    });
 
   return element;
 };
--- ShoppingList/index.js
+++ ShoppingList/index.js
@@ -3,27 +3,58 @@ import { ShoppingItem } from "../ShoppingItem/index.js";
 export const ShoppingList = (props) => {
   const { day, dayName, items } = props;
 
-  const element = document.createElement('div');
-  element.classList.add('shopping-list');
+  const element = document.createElement("div");
+  element.classList.add("shopping-list");
   element.innerHTML = `
     <h2>${dayName}</h2>
+    <form>
+    <input type="text" class="produkt" placeholder="Produkt">
+    <input type="number" class="mnozstvi" placeholder="Množství">
+    <input type="text" class="jednotka" placeholder="Jednotka">
+    <button type="submit">Přidat</button>
+    </form>
     <ul class="shopping-list__items"></ul>
   `;
-  
+
   if (items === undefined) {
-    fetch(`https://apps.kodim.cz/daweb/trening-api/apis/shopping/${day}`)
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/31/days/${day}`)
       .then((response) => response.json())
       .then((data) => {
-        element.replaceWith(ShoppingList({
-          day: day,
-          dayName: dayName,
-          items: data,
-        }));
+        element.replaceWith(
+          ShoppingList({
+            day: day,
+            dayName: dayName,
+            items: data.results,            
+          })
+        );
       });
   } else {
-    const ulElement = element.querySelector('ul');
-    ulElement.append(...items.map((item) => ShoppingItem(item)));
+    const ulElement = element.querySelector("ul");
+    ulElement.append(...items.map((item) => ShoppingItem({...item, day})));
   }
+
+  element.querySelector("form").addEventListener("submit", () => {
+    event.preventDefault();
+    fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/31/days/${day}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        product: document.querySelector(".produkt").value,
+        amount: document.querySelector(".mnozstvi").value,
+        unit: document.querySelector(".jednotka").value,
+        done: false,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        element
+          .querySelector("ul")
+          .replaceWith(...data.results.map((item) => ShoppingItem({...item, day})))
+      );
+  });
 
   return element;
 };
+
