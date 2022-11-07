e736baaa9064ff65141d7ce92f031fc537e2b888
JanaKoduje <jjanule@gmail.com>
2022-11-07 22:13:18 +0100

    pridana uprava mnozstvi v polozce

--- ShoppingItem/index.js
+++ ShoppingItem/index.js
@@ -13,10 +13,26 @@ export const ShoppingItem = (props) => {
   element.classList.add("item");
   element.innerHTML = `
     <div class="item__name">${product}</div>
-    <div class="item__amount">${amount} ${unit}</div>
+    <div class="item__amount"><input id="amount" type="number" value="${amount}"> ${unit}</div>
     <button id="checked" class="item__btn-done ${checkClass}"></button>
     <button id="trash" class="item__btn-delete"></button>
   `;
+
+  const amountElm = element.querySelector("#amount");
+  amountElm.addEventListener("change", () => {
+    fetch(
+      `https://apps.kodim.cz/daweb/shoplist/api/weeks/31/days/${day}/${id}`,
+      {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ amount: Number(amountElm.value) }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) =>
+        element.replaceWith(ShoppingItem({ ...data.results, day }))
+      );
+  });
 
   element.querySelector("#checked").addEventListener("click", () => {
     fetch(
